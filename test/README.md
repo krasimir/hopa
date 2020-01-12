1c3d37e088d4312afb0311c16b1b860c2aa4972e
Krasimir Tsonev <krasimir@outset.ws>
2020-01-12 07:52:20 +0200

    TypeScript support

---
+++ test/README.md
@@ -0,0 +1,8 @@
+A couple of files to test the library.
+
+```
+// in the root folder
+> yarn
+> cd ./test
+> node ../lib/index.js
+```
---
+++ test/typescript.ts
@@ -0,0 +1,25 @@
+const userAccount = {
+  name: "Kieron",
+  id: 0
+}
+const pie = {
+  type: "Apple"
+}
+const purchaseOrder = {
+  owner: userAccount,
+  item: pie
+}
+console.log(purchaseOrder.item.type)
+
+const allOrders = [purchaseOrder]
+
+const firstOrder = allOrders[0]
+console.log(firstOrder.item.type)
+
+const poppedFirstOrder = allOrders.pop()
+
+type PurchaseOrder = typeof purchaseOrder
+
+const readonlyOrders: readonly PurchaseOrder[] = [purchaseOrder]
+
+readonlyOrders.pop();
