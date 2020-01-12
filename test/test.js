b40180ab35eaa7726b747105945253fc913acc2d
Krasimir Tsonev <krasimir@outset.ws>
2020-01-12 05:53:12 +0200

    Probably working :rocket:

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const a = require('./test2.js');
+
+console.log(a());
+
+function * test() {
+  yield 'fooa';
+}
+
+const t = test();
+console.log(t.next());
+
+setInterval(() => {
+  // console.log('fa');dsadas
+}, 1000);
